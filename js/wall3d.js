0ca770c9a4ab94cd306acab87e3f53395568939b
sangha-ju <48509329+sangha-ju@users.noreply.github.com>
2020-03-19 16:39:43 +0900

    character css/js commit

--- js/wall3d.js
+++ js/wall3d.js
@@ -27,4 +27,10 @@
 
     window.addEventListener('resize', resizeHandler);
     resizeHandler();
+
+    stageElem.addEventListener('click', function(e){
+        new Character({
+            xPos: e.clientX / window.innerWidth * 100
+        });
+    })
 })();
